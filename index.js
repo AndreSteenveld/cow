7357150fbce652d800ec3da376bf5dcb9014081a
Andre Steenveld <andre.steenveld@gmail.com>
2017-12-17 16:40:11 +0100

    setting up files

---
+++ index.js
@@ -0,0 +1,4 @@
+#!/bin/sh
+":" //# ; [ "./node_modules/.bin/babel-node" ] && exec /usr/bin/env ./node_modules/.bin/babel-node "$0" "$@" || exec /usr/bin/env node "$0" "$@" ;
+
+require( "./src" ).cli( require( "yargs" ), process.argv );
